18f7783ed0472fbd932315cc3fcae9a919b5fee0
Valeria Meza <valeriameza173@gmail.com>
2021-03-01 23:05:31 -0600

    create basic modal with service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ModalService } from './components/modal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'modal-example';
+  bodyText: string;
+
+  constructor(private modalService: ModalService) {}
+
+  ngOnInit() {
+    this.bodyText = 'This text can be updated in modal 1';
+  }
+
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ModalModule } from './components/modal';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ModalModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
